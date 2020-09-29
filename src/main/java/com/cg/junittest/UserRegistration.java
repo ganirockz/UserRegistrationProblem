2140e5f37171a932a42e287e8ba05d35b71a4de6
ganirockz <ganeshrapeti8@gmail.com>
2020-09-29 20:52:05 +0530

    [Ganesh] Added: Validate last name of user

--- src/main/java/com/cg/junittest/UserRegistration.java
+++ src/main/java/com/cg/junittest/UserRegistration.java
@@ -9,7 +9,7 @@ public class UserRegistration {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to User Registration system");
 		Pattern pattern = Pattern.compile("[A-Z]{1}[a-zA-Z]{2,}");
-		String firstName;
+		String firstName,lastName;
 		while (true) {
 			System.out.println("Enter the first name of user: ");
 			firstName = sc.nextLine();
@@ -20,6 +20,18 @@ public class UserRegistration {
 			}
 			else {
 				System.out.println("Please enter the valid first name");
+			}
+		}
+		while (true) {
+			System.out.println("Enter the last name of user: ");
+			lastName = sc.nextLine();
+			Matcher m = pattern.matcher(lastName);
+			if(m.find()) {
+				System.out.println("User last name added successfully");
+				break;
+			}
+			else {
+				System.out.println("Please enter the valid last name");
 			}
 		}
 		sc.close();
