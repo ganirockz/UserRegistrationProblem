9d1bccd73bb9021d6e6e99d287dc13b2b3491ee9
ganirockz <ganeshrapeti8@gmail.com>
2020-09-29 20:53:58 +0530

    [Ganesh] Added: Validate email of user

--- src/main/java/com/cg/junittest/UserRegistration.java
+++ src/main/java/com/cg/junittest/UserRegistration.java
@@ -9,7 +9,7 @@ public class UserRegistration {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to User Registration system");
 		Pattern pattern = Pattern.compile("[A-Z]{1}[a-zA-Z]{2,}");
-		String firstName,lastName;
+		String firstName,lastName,Email;
 		while (true) {
 			System.out.println("Enter the first name of user: ");
 			firstName = sc.nextLine();
@@ -32,6 +32,19 @@ public class UserRegistration {
 			}
 			else {
 				System.out.println("Please enter the valid last name");
+			}
+		}
+		Pattern p = Pattern.compile("^([a][b][c])[.]([a-zA-Z]+)[@]([b][l][.][c][o])[.]([a-zA-Z]{2})$");
+		while (true) {
+			System.out.println("Enter the Email of user: ");
+			Email = sc.nextLine();
+			Matcher m = p.matcher(Email);
+			if(m.find()) {
+				System.out.println("User Email added successfully");
+				break;
+			}
+			else {
+				System.out.println("Please enter the valid Email");
 			}
 		}
 		sc.close();
