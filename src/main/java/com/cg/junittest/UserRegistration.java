40577c55f096010d1b2301407d178151b9abefd2
ganirockz <ganeshrapeti8@gmail.com>
2020-09-29 20:49:25 +0530

    [Ganesh] Added: Validate first name of user

--- src/main/java/com/cg/junittest/UserRegistration.java
+++ src/main/java/com/cg/junittest/UserRegistration.java
@@ -6,7 +6,23 @@ import java.util.regex.*;
 public class UserRegistration {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to User Registration system");
+		Pattern pattern = Pattern.compile("[A-Z]{1}[a-zA-Z]{2,}");
+		String firstName;
+		while (true) {
+			System.out.println("Enter the first name of user: ");
+			firstName = sc.nextLine();
+			Matcher m = pattern.matcher(firstName);
+			if(m.find()) {
+				System.out.println("User first name added successfully");
+				break;
+			}
+			else {
+				System.out.println("Please enter the valid first name");
+			}
+		}
+		sc.close();
 	}
 
 }
