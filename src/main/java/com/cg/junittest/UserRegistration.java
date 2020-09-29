10771235f184bdb7ee1bd08d83dd43e822980f13
ganirockz <ganeshrapeti8@gmail.com>
2020-09-29 20:40:08 +0530

    Welcome to user registration system

---
+++ src/main/java/com/cg/junittest/UserRegistration.java
@@ -0,0 +1,12 @@
+package com.cg.junittest;
+
+import java.util.*;
+import java.util.regex.*;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration system");
+	}
+
+}
